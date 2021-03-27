68b35ba8fce6bbe5526ef9b462f50de994d3c936
TommyNguyen3921 <55663532+TommyNguyen3921@users.noreply.github.com>
2021-03-26 21:22:42 -0400

    v3

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand navbar-dark bg-dark" aria-label="Second navbar example">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index">Hotel</a>
+      <a class="navbar-brand" href="/">Hotel</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index">Rooms</a>
+            <a class="nav-link active" aria-current="page" href="/">Rooms</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="booking">Bookings</a>
